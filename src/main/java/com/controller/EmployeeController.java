09c204918997494814314c48cb874ff2c1745417
tejas <tejasshah@gmail.com>
2023-10-07 17:50:17 +0530

    file upload

---
+++ src/main/java/com/controller/EmployeeController.java
@@ -0,0 +1,42 @@
+package com.controller;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class EmployeeController {
+
+	@GetMapping("/newemp")
+	public String newEmp() {
+		return "EmployeeRegi";
+	}
+
+	@PostMapping("/uploadprofile")
+	public String uploadProfile(@RequestParam("profile") MultipartFile file) {
+		System.out.println(file.getOriginalFilename());
+
+		// file
+		String mainPath = "D:\\Temp\\spring23";
+
+		File myFile = new File(mainPath, file.getOriginalFilename());
+
+		try {
+			byte b[] = file.getBytes();
+
+			FileUtils.writeByteArrayToFile(myFile, b);
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return "EmployeeRegi";
+	}
+}
